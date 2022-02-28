48a61fee99b45484d66bbe868ba3e12b8a928efc
dupuya <antoine.dupuy@irit.fr>
2022-02-28 15:09:57 +0100

    feat: setup ontology metadata

--- xsd_to_rdf/graph/graph.py
+++ xsd_to_rdf/graph/graph.py
@@ -1,15 +1,15 @@
-from rdflib import ConjunctiveGraph
+from rdflib import ConjunctiveGraph, DC, Literal, RDF, OWL
 from xsd_to_rdf.serializers.graph_seralizers import ObjectSerializer
-from xsd_to_rdf.settings import settings
-
-NAMESPACES = settings.get('ontology').get('namespaces')
+from rdflib.extras.infixowl import Ontology
 
 
 class Graph:
 
-    def __init__(self):
+    def __init__(self, config):
         self.graph = ConjunctiveGraph()
-        for key, value in NAMESPACES.items():
+        self.metadata = config.get('metadata')
+        ns = config.get('options').get('namespaces')
+        for key, value in ns.items():
             self.graph.bind(key, value)
 
     def add_triplet(self, triplet):
@@ -18,6 +18,17 @@ class Graph:
     def bind_namespace(self, prefix, namespace):
         self.graph.bind(prefix, namespace)
 
+    def set_metadata(self):
+        self.graph.bind('dc', DC)
+        print(self.metadata.get('date'))
+        self.graph.add((self.graph.identifier, RDF.type, OWL.Ontology))
+        self.graph.add((self.graph.identifier, OWL.versionInfo, Literal(self.metadata.get('version'))))
+        self.graph.add((self.graph.identifier, DC.title, Literal(self.metadata.get('title'))))
+        self.graph.add((self.graph.identifier, DC.creator, Literal(self.metadata.get('creator'))))
+        self.graph.add((self.graph.identifier, DC.date, Literal(self.metadata.get('date'))))
+        self.graph.add((self.graph.identifier, DC.source, Literal(self.metadata.get('source'))))
+        self.graph.add((self.graph.identifier, DC.language, Literal(self.metadata.get('language'))))
+
     def serialize(self, output, format):
         serializer = ObjectSerializer()
         serializer.serialize(self.graph, output, format)
@@ -25,4 +36,3 @@ class Graph:
     @property
     def get_graph(self):
         return self.graph
-    
